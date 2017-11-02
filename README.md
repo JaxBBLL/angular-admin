2dd540a48a5b808b1426418e0c49be40127cab56
liusc <2851282622@jezoe.com>
2017-11-02 14:56:57 +0800

    添加gulp构建

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # layui-spa
+
+npm install -g gulp
+
+> dev
+gulp
+
+localhost:3000
+
+> build
+gulp build
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,49 @@
 var gulp = require('gulp');
-var connect = require('gulp-connect'); //构建本地Server服务的组件
-var uglify = require('gulp-uglify'); // js压缩
-var fs = require('fs'); //Node.js的文件系统的Api
+var connect = require('gulp-connect');
+var uglify = require('gulp-uglify');
+var fs = require('fs');
 var babel = require('gulp-babel');
 var cleanCSS = require('gulp-clean-css');
-var proxy = require('gulp-connect-proxy'); //跨域插件
+var Proxy = require('gulp-connect-proxy');
 var stripDebug = require('gulp-strip-debug');
+
+var watchDes = {
+  html: './src/**/*.html',
+  js: './src/**/*.js',
+  css: './src/**/*.css'
+}
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'src',
+    port: 3000,
+    livereload: true
+      // ,middleware: function(connect, opt) {
+      //     opt.route = '/proxy';
+      //     var proxy = new Proxy(opt);
+      //     return [proxy];
+      // }
+  });
+});
+
+gulp.task('html', function() {
+  gulp.src(watchDes.html)
+    .pipe(connect.reload());
+});
+
+gulp.task('js', function() {
+  gulp.src(watchDes.js)
+    .pipe(connect.reload());
+});
+
+gulp.task('css', function() {
+  gulp.src(watchDes.css)
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch([watchDes.html, watchDes.js, watchDes.css], ['html', 'js']);
+});
+
+
+gulp.task('default', ['connect', 'watch']);
