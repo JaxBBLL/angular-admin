cf0413a349057afbaaf2322a46272e23d897c5e9
liusc <2851282622@jezoe.com>
2017-11-02 14:14:26 +0800

    update router setting

--- js/public.js
+++ js/public.js
@@ -14,6 +14,9 @@
     'user': [{
       path: '/one',
       title: '第一个页面'
+    }, {
+      path: '/two',
+      title: '第二个页面'
     }]
   }
   var routerMap = [{
@@ -25,13 +28,13 @@
     path: '/two',
     page: '/views/two.html',
     title: '第二个页面',
-    parent: '设置'
+    parent: '设置',
+    menu: 'user'
   }, {
     path: '/three',
     page: '/views/three.html',
     title: '第三个页面',
-    parent: '设置',
-    menu: 'user'
+    parent: '设置'
   }, {
     path: '/404',
     page: '/views/404.html',
@@ -41,9 +44,13 @@
 
   var routeData = routeSet(routerMap);
 
-  
+
   $router = Router(routeData).configure({
     before: function() {
+      console.log('before...')
+    },
+    after: function() {
+      console.log('after...')
     },
     notfound: function() {
       console.log('notfound')
@@ -58,22 +65,28 @@
     for (var i = 0; i < routerMap.length; i++) {
       var _cb = [];
       var _menu = routerMap[i].menu || 'default';
-      _cb.push(renderPage(routerMap[i].page, routerMap[i].title));
-      _cb.push(renderMenu(_menu))
+      var _path = routerMap[i].path;
+      var _page = routerMap[i].page;
+      var _title = routerMap[i].title;
+      _cb.push(renderPage(_page, _title, _path));
+      _cb.push(renderMenu(_menu, _path))
       result[routerMap[i].path] = _cb;
     }
     return result;
   }
 
   //  路由更改渲染对应的页面
-  function renderPage(page, title) {
+  function renderPage(page, title, path) {
     return function() {
+      console.log('on...')
       document.title = title;
       $('#router-view').load(page + '?v=' + new Date().getTime())
+      $('#app-menu a').removeClass('router-active')
+      $('#app-menu a[href="#'+path+'"]').addClass('router-active')
     }
   }
 
-  function renderMenu(name) {
+  function renderMenu(name,path) {
     return function() {
       var menuName = $('#app-menu').data('menu')
       if (menuName === name) {
@@ -82,7 +95,13 @@
       var menu = menus[name];
       var html = '';
       $.each(menu, function(i, item) {
-        html += '<a href="#' + item.path + '">' + item.title + '</a>'
+        var h;
+        if (item.path === path) {
+          h = '<a href="#' + item.path + '" class="router-active">' + item.title + '</a>'
+        } else {
+          h = '<a href="#' + item.path + '">' + item.title + '</a>'
+        }
+        html += h;
       })
       $('#app-menu').html(html)
       $('#app-menu').data('menu', name)
